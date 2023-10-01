e18467c34544456a58d38707194609d9be6904b3
James Ragive Dominique <72082423+dondodo1985@users.noreply.github.com>
2023-09-30 22:04:04 -0400

    my9th commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,20 +3,19 @@ import FooterNav from "./footerNav";
 import UtilityComponent from "./utilityFooterComponet";
 
 const Footer = (props) => {
-  const customFooterStyle = {
-    zIndex: "10",
-  };
-  return (
-    <div
-      className="m-auto d-flex align-items-center justify-content-center "
-      style={customFooterStyle}
-    >
-      <UtilityComponent tags={props.utilityTags}/>
-      <footer className="shadow-lg d-flex align-items-center justify-content-center footer-nav-container">
-        <FooterNav onClick={props.onClick}/>
-      </footer>
-    </div>
-  );
+	const customFooterStyle = {
+		zIndex: "10",
+	};
+	return (
+		<div
+			className="m-auto d-flex align-items-center justify-content-center "
+			style={customFooterStyle}>
+			<UtilityComponent tags={props.utilityTags} />
+			<footer className="shadow-lg d-flex align-items-center justify-content-center footer-nav-container">
+				<FooterNav onClick={props.onClick} />
+			</footer>
+		</div>
+	);
 };
 
 export default Footer;
